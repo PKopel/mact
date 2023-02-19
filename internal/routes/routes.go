d639cf5f470d78d53d632a190f8f3aa8a543a8e7
Paweł Kopel <pawel.kopel2@gmail.com>
2023-02-19 16:32:24 +0100

    Rewrite server to use gorilla instead of gin

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -2,13 +2,14 @@ package routes
 
 import (
 	"encoding/json"
+	"io"
+	"log"
 	"net/http"
 
 	conf "github.com/PKopel/mact/internal/config"
 	mact "github.com/PKopel/mact/internal/json"
 	"github.com/PKopel/mact/internal/utils"
-
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 )
 
 func copyHeader(dst, src http.Header) {
@@ -19,50 +20,76 @@ func copyHeader(dst, src http.Header) {
 	}
 }
 
-func setupEndopint(endpoint conf.EndpointConfig, host string) func(*gin.Context) {
-	return func(c *gin.Context) {
-		request, err := http.NewRequest(string(endpoint.Verb), host+endpoint.Path, c.Request.Body)
+func setupEndopint(endpoint conf.EndpointConfig, host string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		server := host + r.URL.Path
+		request, err := http.NewRequest(string(endpoint.Verb), server, r.Body)
 		if err != nil {
-			panic(err)
+			log.Fatalf("Error while creating request: %v", err)
 		}
-		copyHeader(request.Header, c.Request.Header)
+		copyHeader(request.Header, r.Header)
 
+		log.Printf("Sending request to %v", server)
 		client := &http.Client{}
 		resp, err := client.Do(request)
 		if err != nil {
-			panic(err)
+			log.Fatalf("Error while making request: %v", err)
 		}
 
 		var body mact.JSON
 		err = json.NewDecoder(resp.Body).Decode(&body)
 		if err != nil {
-			panic(err)
+			log.Fatalf("Error while decoding response: %v", err)
 		}
 
 		if utils.Contains(endpoint.StatusCodes, resp.StatusCode) {
 			body = mact.ApplyChanges(body, endpoint.Changes)
 		}
 
-		c.JSON(resp.StatusCode, body)
+		log.Printf("Writing response body: %v", body)
+
+		copyHeader(w.Header(), resp.Header)
+		w.WriteHeader(resp.StatusCode)
+		json.NewEncoder(w).Encode(body)
 	}
 }
 
-func SetupRouter(router *gin.Engine, config conf.MactConfig) {
+func setupIgnored(host string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		server := host + r.URL.Path
+		request, err := http.NewRequest(r.Method, host+r.URL.Path, r.Body)
+		if err != nil {
+			log.Fatalf("Error while creating request: %v", err)
+		}
+		copyHeader(request.Header, r.Header)
+
+		log.Printf("Sending request to %v", server)
+
+		client := &http.Client{}
+		resp, err := client.Do(request)
+		if err != nil {
+			log.Fatalf("Error while making request: %v", err)
+		}
+
+		copyHeader(w.Header(), resp.Header)
+		w.WriteHeader(resp.StatusCode)
+		io.Copy(w, resp.Body)
+	}
+}
+
+func SetupRouter(router *mux.Router, config conf.MactConfig) {
+
 	for _, service := range config.Services {
+		log.Printf("Setting up service %v", service.Host)
+		// setup endpoints to be processed
 		for _, endpoint := range service.Endpoints {
 			handlerFunc := setupEndopint(endpoint, service.Host)
-			switch endpoint.Verb {
-			case conf.GET:
-				router.GET(endpoint.Path, handlerFunc)
-			case conf.PUT:
-				router.PUT(endpoint.Path, handlerFunc)
-			case conf.POST:
-				router.POST(endpoint.Path, handlerFunc)
-			case conf.PATCH:
-				router.PATCH(endpoint.Path, handlerFunc)
-			case conf.DELETE:
-				router.DELETE(endpoint.Path, handlerFunc)
-			}
+			router.Path(endpoint.Path).HandlerFunc(handlerFunc).Methods(string(endpoint.Verb))
+			log.Printf("Setting up endpoint %v", endpoint.Path)
 		}
+		// setup endpoints to be ignored
+		handlerFunc := setupIgnored(service.Host)
+		router.PathPrefix("/").HandlerFunc(handlerFunc)
+
 	}
 }
--- main.go
+++ main.go
@@ -2,23 +2,31 @@ package main
 
 import (
 	"flag"
+	"log"
+	"net/http"
+	"time"
 
 	conf "github.com/PKopel/mact/internal/config"
 	"github.com/PKopel/mact/internal/routes"
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
 )
 
 var configFile = flag.String("config", "config.yaml", "Path to config file")
 var serverPort = flag.String("port", "8000", "Port to listen on")
-var debugMode = flag.Bool("debug", false, "Enable debug mode")
 
 func main() {
 	flag.Parse()
-	if !*debugMode {
-		gin.SetMode(gin.ReleaseMode)
-	}
-	router := gin.Default()
+	log.SetPrefix("[MA/CT]")
+	router := mux.NewRouter()
 	config := conf.ReadConfig(*configFile)
 	routes.SetupRouter(router, config)
-	router.Run(":" + *serverPort)
+
+	srv := &http.Server{
+		Handler:      router,
+		Addr:         "0.0.0.0:" + *serverPort,
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
